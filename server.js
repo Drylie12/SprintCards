eab6090c7defee7764b8c4e4e31de89676f3f6a1
Chris Drylie <40637442+Drylie12@users.noreply.github.com>
2021-05-04 09:29:57 +1000

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const nextHandler = nextApp.getRequestHandler()
 
-let port = 80
+let port = 90
 
 let answers = {};
 
